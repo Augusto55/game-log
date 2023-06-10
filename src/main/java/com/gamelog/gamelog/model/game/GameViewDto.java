b4a3fe7b9a03a72959580dde1c1f25c056e57e35
Augusto55 <85200864+Augusto55@users.noreply.github.com>
2023-06-10 00:05:00 -0300

    Restricted return

---
+++ src/main/java/com/gamelog/gamelog/model/game/GameViewDto.java
@@ -0,0 +1,27 @@
+package com.gamelog.gamelog.model.game;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GameViewDto {
+
+    String name;
+    LocalDate launchDate;
+    String developer;
+    String publisher;
+    String genres;
+
+    public GameViewDto (Game game){
+        this.name  = game.getName();
+        this.launchDate = game.getLaunchDate();
+        this.developer = game.getDeveloper();
+        this.publisher = game.getPublisher();
+        this.genres = game.getGenres();
+    }
+}
